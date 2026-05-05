12b048ed0635aa3a2220f427eb8a1d73cb01dc7c
cristianriascos09-lang <cristianriascos09@gmail.com>
2026-05-04 19:08:32 -0500

    cambio2

--- .githut/workflows/conexion.php
+++ .githut/workflows/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "calculo");
+$conexion = new mysqli(" mysql-cristian1995.alwaysdata.net", "cristian1995", "", "clase123");
 
 if ($conexion->connect_error) {
     die("Error de conexión");
